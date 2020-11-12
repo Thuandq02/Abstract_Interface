760b351db7774facca1f3807b4b05a45e0ef63bd
dqthua <duongquangthuan2002@gmail.com>
2020-11-12 20:19:05 +0700

    add file

---
+++ TH3/ComparableCircle.php
@@ -0,0 +1,26 @@
+<?php
+include "Circle.php";
+include "Comparable.php";
+
+class ComparableCircle extends Circle implements Comparable
+{
+    public function __construct($name, $radius)
+    {
+        parent::__construct($name, $radius);
+    }
+
+    public function compareto($objOther)
+    {
+        $circleOtherRadius = $objOther->getRadius();
+        if ($this->getRadius() > $circleOtherRadius){
+            return 1;
+        }elseif ($this->getRadius() < $circleOtherRadius){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+
+
+
+}
